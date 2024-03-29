d0f392093e2be4b88a36e90ae126a3f858930fbb
Geraldo Daros <geraldodarosfilho@hotmail.com>
2024-03-29 12:25:05 -0300

    create user and transfer indexes

---
+++ src/main/resources/db/migration/V3__create_user_index.sql
@@ -0,0 +1,2 @@
+CREATE INDEX idx_users_email ON users (email);
+CREATE INDEX idx_users_document ON users (document);
---
+++ src/main/resources/db/migration/V4__create_transfer_index.sql
@@ -0,0 +1,2 @@
+CREATE INDEX idx_transfer_user_from_fk ON transfer (user_from_fk);
+CREATE INDEX idx_transfer_user_to_fk ON transfer (user_to_fk);
