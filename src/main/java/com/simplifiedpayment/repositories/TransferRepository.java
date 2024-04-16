098eac24aaabad42ebafb44d4b9cd4207951124e
Geraldo Daros <geraldodarosfilho@hotmail.com>
2024-04-15 23:50:53 -0300

    Add missing annotations.

--- src/main/java/com/simplifiedpayment/repositories/TransferRepository.java
+++ src/main/java/com/simplifiedpayment/repositories/TransferRepository.java
@@ -2,6 +2,8 @@ package com.simplifiedpayment.repositories;
 
 import com.simplifiedpayment.data.models.Transfer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TransferRepository extends JpaRepository<Transfer, Long> {
 }
