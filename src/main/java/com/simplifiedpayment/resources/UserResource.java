4a070c72240c2384436e947f527bcb123e37fbcd
Geraldo Daros <geraldodarosfilho@hotmail.com>
2024-05-08 22:19:53 -0300

    add logs with Log4j to track requests

--- src/main/java/com/simplifiedpayment/resources/UserResource.java
+++ src/main/java/com/simplifiedpayment/resources/UserResource.java
@@ -9,6 +9,8 @@ import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
@@ -17,6 +19,7 @@ import java.util.List;
 public class UserResource {
 
     private final UserService userService;
+    private static final Logger logger = LoggerFactory.getLogger(UserResource.class);
 
     public UserResource(UserService userService) {
         this.userService = userService;
@@ -24,41 +27,65 @@ public class UserResource {
 
     @GetMapping
     public List<ReadUserDTO> getAll() {
+        logger.info("Requisição para buscar todos os usuários.");
         return this.userService.getAll();
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<User> getOneById(@PathVariable("id") Long id) {
+        logger.info("Requisição para buscar usuário por ID: {}", id);
         User user = this.userService.getOneById(id);
-        if (user == null) return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        if (user == null) {
+            logger.error("Usuário com ID {} não encontrado.", id);
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
         return ResponseEntity.ok(user);
     }
 
     @GetMapping("/document")
     public ResponseEntity<User> getOneByDocument(@RequestParam("document") String document) {
+        logger.info("Requisição para buscar usuário por documento: {}", document);
         User user = this.userService.getOneByDocument(document);
-        if (user == null) return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        if (user == null) {
+            logger.warn("Usuário com documento {} não encontrado.", document);
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
         return ResponseEntity.ok(user);
     }
 
     @PostMapping
     public ResponseEntity<Void> create(@RequestBody @Valid CreateUserDTO userDto) {
+        logger.info("Requisição para criar um novo usuário.");
         User user = this.userService.create(userDto);
-        if (user == null) return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        if (user == null) {
+            logger.error("Falha ao criar o usuário.");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+        logger.info("Usuário criado com sucesso. ID: {}", user.getId());
         return ResponseEntity.status(HttpStatus.CREATED).body(null);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<Void> update(@PathVariable("id") Long id, @RequestBody UpdateUserDTO userDTO) {
-        User createdUser = this.userService.update(id, userDTO);
-        if (createdUser == null) return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        logger.info("Requisição para atualizar usuário com ID: {}", id);
+        User updatedUser = this.userService.update(id, userDTO);
+        if (updatedUser == null) {
+            logger.error("Usuário com ID {} não encontrado para atualização.", id);
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+        logger.info("Usuário atualizado com sucesso. ID: {}", updatedUser.getId());
         return ResponseEntity.status(HttpStatus.OK).body(null);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable("id") Long id) {
+        logger.info("Requisição para deletar usuário com ID: {}", id);
         boolean deleted = this.userService.delete(id);
-        if(!deleted) return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        if (!deleted) {
+            logger.error("Usuário com ID {} não encontrado para deleção.", id);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+        logger.info("Usuário deletado com sucesso. ID: {}", id);
         return ResponseEntity.status(HttpStatus.OK).body(null);
     }
 }
--- src/main/java/com/simplifiedpayment/services/UserService.java
+++ src/main/java/com/simplifiedpayment/services/UserService.java
@@ -9,65 +9,92 @@ import com.simplifiedpayment.repositories.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
 public class UserService {
+
     private final UserRepository userRepository;
+    private static final Logger logger = LoggerFactory.getLogger(UserService.class);
 
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
     public List<ReadUserDTO> getAll() {
+        logger.info("Buscando todos os usuários.");
         return this.userRepository.findAll().parallelStream()
                 .map(ReadUserDTO::userToReadDTO)
                 .collect(Collectors.toList());
     }
 
     public User getOneById(Long id) {
+        logger.info("Buscando usuário por ID: {}", id);
         Optional<User> optionalUser = this.userRepository.findById(id);
         boolean isPresent = optionalUser.isPresent();
-        return isPresent ? optionalUser.get() : null;
+        if (!isPresent) {
+            logger.warn("Usuário com ID {} não encontrado.", id);
+            return null;
+        }
+        return optionalUser.get();
     }
 
     public User create(CreateUserDTO userDto) {
+        logger.info("Criando novo usuário.");
         User user = userDto.toUser();
         user.setPassword(new BCryptPasswordEncoder().encode(userDto.password()));
         return this.userRepository.save(user);
     }
 
     public User update(Long id, UpdateUserDTO userDTO) {
+        logger.info("Atualizando usuário com ID: {}", id);
         User user = this.getOneById(id);
-        if (user == null) return null;
+        if (user == null) {
+            logger.warn("Usuário com ID {} não encontrado para atualização.", id);
+            return null;
+        }
         User userToSave = userDTO.updateUser(user);
         return this.userRepository.save(userToSave);
     }
 
     public boolean delete(Long id) {
+        logger.info("Deletando usuário com ID: {}", id);
         User userToDelete = this.getOneById(id);
-        if (userToDelete == null) return false;
+        if (userToDelete == null) {
+            logger.warn("Usuário com ID {} não encontrado para deleção.", id);
+            return false;
+        }
         this.userRepository.delete(userToDelete);
+        logger.info("Usuário deletado com sucesso. ID: {}", id);
         return true;
     }
 
     public void updateWallet(User user) {
+        logger.info("Atualizando carteira do usuário com ID: {}", user.getId());
         User userToUpdate = getOneById(user.getId());
         userToUpdate.setWallet(user.getWallet());
         userRepository.save(userToUpdate);
+        logger.info("Carteira do usuário atualizada com sucesso. ID: {}", user.getId());
     }
 
     public User getOneByDocument(String document) {
+        logger.info("Buscando usuário por documento: {}", document);
         Optional<User> optionalUser = this.userRepository.findByDocument(document);
         boolean isPresent = optionalUser.isPresent();
-        return isPresent ? optionalUser.get() : null;
+        if (!isPresent) {
+            logger.warn("Usuário com documento {} não encontrado.", document);
+            return null;
+        }
+        return optionalUser.get();
     }
 
     public UserDetails getOneByEmail(String document) {
+        logger.info("Buscando usuário por email: {}", document);
         return this.userRepository.findByEmail(document);
     }
 }
